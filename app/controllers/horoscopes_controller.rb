7cf8ec29f695f9521aa2882118191c5b9380b72d
Ivan <mrkatakin@gmail.com>
2013-03-01 14:21:45 +1000

    horoscopes

--- app/controllers/horoscopes_controller.rb
+++ app/controllers/horoscopes_controller.rb
@@ -2,109 +2,82 @@ class HoroscopesController < ApplicationController
   # GET /horoscopes
   # GET /horoscopes.json
   def index
-    require 'open-uri'
-    require 'nokogiri'
-    info = []
+    @horoscopes = Horoscope.all
 
-    vodoley = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/aquarius/"))
-    info << vodoley.at_css('div.horo-body').text.strip
-    ryby = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/pisces/"))
-    info << ryby.at_css('div.horo-body').text.strip
-    oven = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/aries/"))
-    info << oven.at_css('div.horo-body').text.strip
-    telets = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/taurus/"))
-    info << telets.at_css('div.horo-body').text.strip
-    bliznetsy = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/gemini/"))
-    info << bliznetsy.at_css('div.horo-body').text.strip
-    rak = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/cancer/"))
-    info << rak.at_css('div.horo-body').text.strip
-    lev = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/leo/"))
-    info << lev.at_css('div.horo-body').text.strip
-    deva = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/virgo/"))
-    info << deva.at_css('div.horo-body').text.strip
-    vesy = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/libra/"))
-    info << vesy.at_css('div.horo-body').text.strip
-    skorpion = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/scorpio/"))
-    info << skorpion.at_css('div.horo-body').text.strip
-    strelets = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/sagittarius/"))
-    info << strelets.at_css('div.horo-body').text.strip
-    kozerog = Nokogiri::HTML(open("http://m.horoscopes.rambler.ru/capricorn/"))
-    info << kozerog.at_css('div.horo-body').text.strip
-    
     respond_to do |format|
       #format.html # index.html.erb
-      format.json { render json: Oj.dump(info) }
+      format.json { render json: Oj.dump((@horoscopes, :mode => :compat) }
     end  
   end
 
   # GET /horoscopes/1
   # GET /horoscopes/1.json
-  def show
-    @horoscope = Horoscope.find(params[:id])
+  # def show
+  #   @horoscope = Horoscope.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @horoscope }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.json { render json: @horoscope }
+  #   end
+  # end
 
-  # GET /horoscopes/new
-  # GET /horoscopes/new.json
-  def new
-    @horoscope = Horoscope.new
+  # # GET /horoscopes/new
+  # # GET /horoscopes/new.json
+  # def new
+  #   @horoscope = Horoscope.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @horoscope }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @horoscope }
+  #   end
+  # end
 
-  # GET /horoscopes/1/edit
-  def edit
-    @horoscope = Horoscope.find(params[:id])
-  end
+  # # GET /horoscopes/1/edit
+  # def edit
+  #   @horoscope = Horoscope.find(params[:id])
+  # end
 
-  # POST /horoscopes
-  # POST /horoscopes.json
-  def create
-    @horoscope = Horoscope.new(params[:horoscope])
+  # # POST /horoscopes
+  # # POST /horoscopes.json
+  # def create
+  #   @horoscope = Horoscope.new(params[:horoscope])
 
-    respond_to do |format|
-      if @horoscope.save
-        format.html { redirect_to @horoscope, notice: 'Horoscope was successfully created.' }
-        format.json { render json: @horoscope, status: :created, location: @horoscope }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @horoscope.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @horoscope.save
+  #       format.html { redirect_to @horoscope, notice: 'Horoscope was successfully created.' }
+  #       format.json { render json: @horoscope, status: :created, location: @horoscope }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @horoscope.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # PUT /horoscopes/1
-  # PUT /horoscopes/1.json
-  def update
-    @horoscope = Horoscope.find(params[:id])
+  # # PUT /horoscopes/1
+  # # PUT /horoscopes/1.json
+  # def update
+  #   @horoscope = Horoscope.find(params[:id])
 
-    respond_to do |format|
-      if @horoscope.update_attributes(params[:horoscope])
-        format.html { redirect_to @horoscope, notice: 'Horoscope was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @horoscope.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @horoscope.update_attributes(params[:horoscope])
+  #       format.html { redirect_to @horoscope, notice: 'Horoscope was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @horoscope.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /horoscopes/1
-  # DELETE /horoscopes/1.json
-  def destroy
-    @horoscope = Horoscope.find(params[:id])
-    @horoscope.destroy
+  # # DELETE /horoscopes/1
+  # # DELETE /horoscopes/1.json
+  # def destroy
+  #   @horoscope = Horoscope.find(params[:id])
+  #   @horoscope.destroy
 
-    respond_to do |format|
-      format.html { redirect_to horoscopes_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to horoscopes_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 end
--- app/models/horoscope.rb
+++ app/models/horoscope.rb
@@ -1,6 +1,88 @@
 class Horoscope
-  include Mongoid::Document
-  field :sign, type: String
-  field :desc, type: String
-  field :den, type: Time
+	include Mongoid::Document
+	field :kozerog, type: String
+	field :vodoley, type: String
+	field :ryby, type: String
+	field :oven, type: String
+	field :telets, type: String
+	field :bliznetsy, type: String
+	field :rak type: String
+	field :lev, type: String
+	field :deva, type: String
+	field :vesy, type: String
+	field :scorpion, type: String
+	field :strelets, type: String
+	field :den, type: Time
+
+	index({ date: 1 }, { unique: true, background: true })
+
+	validates :kozerog, presence: true
+	validates :vodoley, presence: true
+	validates :ryby, presence: true
+	validates :oven, presence: true
+	validates :telets, presence: true
+	validates :bliznetsy, presence: true
+	validates :rak, presence: true
+	validates :lev, presence: true
+	validates :deva, presence: true
+	validates :vesy, presence: true
+	validates :scorpion, presence: true
+	validates :strelets, presence: true
+
+	require 'open-uri'
+	require 'nokogiri'
+
+    def openhtml(url)
+    	# open url with exceptions
+    	failed = true
+    	begin
+    		html = open(url)
+			failed = false                                                 
+			rescue OpenURI::HTTPError => e                                   
+				error_message = e.message                                      
+				response_message = "Response Code = #{e.io.status[0]}"         
+			rescue SocketError => e                                          
+				error_message = e.message                                      
+				response_message = "Host unreachable"                          
+			rescue => e                                                      
+				error_message = e.message                                      
+				response_message = "Unknown error"
+		end                                                              
+		if failed
+	        html = []
+	    end
+
+      	return html
+    end
+
+    #parser
+    kozerog = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/capricorn/"))
+	vodoley = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/aquarius/"))
+	ryby = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/pisces/"))
+	oven = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/aries/"))
+	telets = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/taurus/"))
+	bliznetsy = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/gemini/"))
+	rak = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/cancer/"))
+	lev = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/leo/"))
+	deva = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/virgo/"))
+	vesy = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/libra/"))
+	skorpion = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/scorpio/"))
+	strelets = Nokogiri::HTML(openhtml("http://m.horoscopes.rambler.ru/sagittarius/"))
+
+	@horoscopes
+
+    @info = []
+    info << kozerog.at_css('div.horo-body').text.strip
+    info << vodoley.at_css('div.horo-body').text.strip
+    info << ryby.at_css('div.horo-body').text.strip
+    info << oven.at_css('div.horo-body').text.strip
+    info << telets.at_css('div.horo-body').text.strip
+    info << bliznetsy.at_css('div.horo-body').text.strip
+    info << rak.at_css('div.horo-body').text.strip
+    info << lev.at_css('div.horo-body').text.strip
+    info << deva.at_css('div.horo-body').text.strip
+    info << vesy.at_css('div.horo-body').text.strip
+    info << scorpion.at_css('div.horo-body').text.strip
+    info << strelets.at_css('div.horo-body').text.strip
+
 end
