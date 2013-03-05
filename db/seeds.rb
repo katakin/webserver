aa3ce68f0ad19883b6d0d19a9a0163b314a78679
kolasss <kolas.batman@gmail.com>
2013-03-05 15:55:20 +1000

    changed db seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,25 @@
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
 
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.create!({ :name => role }, :without_protection => true)
-  puts 'role: ' << role
-end
-puts 'DEFAULT USERS'
-user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+# puts 'ROLES'
+# YAML.load(ENV['ROLES']).each do |role|
+#   Role.create!({ :name => role }, :without_protection => true)
+#   puts 'role: ' << role
+# end
+# puts 'DEFAULT USERS'
+# user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+# puts 'user: ' << user.name
+# user.add_role :admin
+
+puts 'CREATING ROLES'
+Role.create([
+  { :name => 'admin' }, 
+  { :name => 'user' }
+], :without_protection => true)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please123', :password_confirmation => 'please123'
+puts 'New user created: ' << user.name
+user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please123', :password_confirmation => 'please123'
+puts 'New user created: ' << user2.name
 user.add_role :admin
+user2.add_role :user
